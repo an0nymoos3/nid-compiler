72539152d57920491647c2a44ec7813ebd181408
an0nymoos3 <moosely123@gmail.com>
2024-04-25 00:28:34 +0200

    arithmetic files

---
+++ compiler/src/compiler/ass_gen/arithmetic.rs
@@ -0,0 +1,12 @@
+/*
+* Handles arithmetic generation. Such as generating the necessary algorithms to make multiplication and
+* division work.
+*/
+
+fn addition() {}
+
+fn subtraction() {}
+
+fn multiplication() {}
+
+fn division() {}
--- compiler/src/compiler/ass_gen/mod.rs
+++ compiler/src/compiler/ass_gen/mod.rs
@@ -1,3 +1,4 @@
+pub mod arithmetic;
 pub mod inliner;
 pub mod instruction_parser;
 pub mod memory_manager;
